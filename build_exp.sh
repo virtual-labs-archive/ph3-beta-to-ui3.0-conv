0a4a187a2cbf5dfecd5992aaf9bd75cfd953668e
ojas1 <mohrilop@gmail.com>
2020-06-11 19:57:15 +0530

    quotes

--- build_exp.sh
+++ build_exp.sh
@@ -42,23 +42,23 @@ cd exprepos/$reponame; git fetch --all; git checkout $tag; cd ../../
 template=ui3template
 
 ## conversion
-mkdir -p expbuilds/$EXP_SHORT_NAME
-cp -r $template/* 'expbuilds/$EXP_SHORT_NAME'
+mkdir -p "expbuilds/$EXP_SHORT_NAME"
+cp -r $template/* "expbuilds/$EXP_SHORT_NAME"
 
-find expbuilds/$EXP_SHORT_NAME/*.html -maxdepth 1 -type f -exec sed -i "s/{{BROAD_AREA}}/$BROAD_AREA/g" {} +
-find expbuilds/$EXP_SHORT_NAME/*.html -maxdepth 1 -type f -exec sed -i "s/{{LAB_NAME}}/$DISPLAY_LAB_NAME/g" {} +
-find expbuilds/$EXP_SHORT_NAME/*.html -maxdepth 1 -type f -exec sed -i "s/{{COLLEGE_NAME}}/$COLLEGE_NAME/g" {} +
-find expbuilds/$EXP_SHORT_NAME/*.html -maxdepth 1 -type f -exec sed -i "s/{{PHASE}}/$PHASE/g" {} +
-find expbuilds/$EXP_SHORT_NAME/*.html -maxdepth 1 -type f -exec sed -i "s/{{EXP_NAME}}/$EXP_NAME/g" {} +
-find expbuilds/$EXP_SHORT_NAME/*.html -maxdepth 1 -type f -exec sed -i "s/{{EXP_SHORT_NAME}}/$EXP_SHORT_NAME/g" {} +
-find expbuilds/$EXP_SHORT_NAME/*.html -maxdepth 1 -type f -exec sed -i "s,{{BA_LINK}},$BROAD_AREA_LINK,g" {} +
-find expbuilds/$EXP_SHORT_NAME/*.html -maxdepth 1 -type f -exec sed -i "s,{{LAB_LINK}},$LAB_LINK,g" {} +
+find "expbuilds/$EXP_SHORT_NAME/*.html" -maxdepth 1 -type f -exec sed -i "s/{{BROAD_AREA}}/$BROAD_AREA/g" {} +
+find "expbuilds/$EXP_SHORT_NAME/*.html" -maxdepth 1 -type f -exec sed -i "s/{{LAB_NAME}}/$DISPLAY_LAB_NAME/g" {} +
+find "expbuilds/$EXP_SHORT_NAME/*.html" -maxdepth 1 -type f -exec sed -i "s/{{COLLEGE_NAME}}/$COLLEGE_NAME/g" {} +
+find "expbuilds/$EXP_SHORT_NAME/*.html" -maxdepth 1 -type f -exec sed -i "s/{{PHASE}}/$PHASE/g" {} +
+find "expbuilds/$EXP_SHORT_NAME/*.html" -maxdepth 1 -type f -exec sed -i "s/{{EXP_NAME}}/$EXP_NAME/g" {} +
+find "expbuilds/$EXP_SHORT_NAME/*.html" -maxdepth 1 -type f -exec sed -i "s/{{EXP_SHORT_NAME}}/$EXP_SHORT_NAME/g" {} +
+find "expbuilds/$EXP_SHORT_NAME/*.html" -maxdepth 1 -type f -exec sed -i "s,{{BA_LINK}},$BROAD_AREA_LINK,g" {} +
+find "expbuilds/$EXP_SHORT_NAME/*.html" -maxdepth 1 -type f -exec sed -i "s,{{LAB_LINK}},$LAB_LINK,g" {} +
 
-cp -r exprepos/$reponame expbuilds/$EXP_SHORT_NAME
-mv expbuilds/$EXP_SHORT_NAME/$reponame 'expbuilds/$EXP_SHORT_NAME/round-template'
+cp -r exprepos/$reponame "expbuilds/$EXP_SHORT_NAME"
+mv "expbuilds/$EXP_SHORT_NAME/$reponame" "expbuilds/$EXP_SHORT_NAME/round-template"
 
-cp fixpointer.py 'expbuilds/$EXP_SHORT_NAME/round-template/experiment/simulation/'
-cd 'expbuilds/$EXP_SHORT_NAME/round-template/experiment/simulation/'
+cp fixpointer.py "expbuilds/$EXP_SHORT_NAME/round-template/experiment/simulation/"
+cd "expbuilds/$EXP_SHORT_NAME/round-template/experiment/simulation/"
 python fixpointer.py
 cp -r images ../../../
 cd ..
