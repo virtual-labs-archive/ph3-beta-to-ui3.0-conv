bac68d7cf2ab33877d995f780f444800d5898f79
ojas1 <mohrilop@gmail.com>
2020-06-12 09:45:34 +0530

    brackets in name

--- host_experiments.py
+++ host_experiments.py
@@ -1,11 +1,12 @@
 import json
 import os
+import re
 
 from config import *
 from utils import *
 
 def build_exp(exp, broad_area_name, display_lab_name, lab_name, college_name, phase, broad_area_link, lab_link):    
-    script_cmd = "./%s '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' %s %s" % (BUILD_SCRIPT, exp["repo"], exp["tag"], exp['short-name'], exp['name'], ".", broad_area_name, display_lab_name, lab_name, college_name, phase, broad_area_link, lab_link)
+    script_cmd = "./%s '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s' '%s'" % (BUILD_SCRIPT, exp["repo"], exp["tag"], exp['short-name'], exp['name'], ".", broad_area_name, display_lab_name, lab_name, college_name, phase, broad_area_link, lab_link)
     try:
         (ret_code, output) = execute_command(script_cmd)
         print "script run successful"
@@ -16,7 +17,7 @@ def build_exp(exp, broad_area_name, display_lab_name, lab_name, college_name, ph
     return exp['repo']
 
 def copy_exps_to_server(build_loc, exp_copy_loc):
-    rsync_cmd = "rsync -a %s %s" % (build_loc, exp_copy_loc)
+    rsync_cmd = "mkdir -p '%s'; rsync -a '%s' '%s'" % (exp_copy_loc, build_loc, exp_copy_loc)
     try:
         (ret_code, output) = execute_command(rsync_cmd)
         print "copy to remote successful"
@@ -30,7 +31,7 @@ def host_experiments():
 
     for exp in data["experiments"]:
         if (exp["deploy"]):
-            lab_name = data["lab"].lower().replace(" ", "-")
+            lab_name = re.sub(r' +', '-', data["lab"].lower())
             lab_link = os.path.join(data["baseUrl"], lab_name)
             exp_copy_loc = "/var/www/html/" + lab_name + "/exp/"
             build_exp(exp, data["broadArea"]["name"], data["lab"], lab_name, data['collegeName'], data['phase'], data["broadArea"]["link"], lab_link)
